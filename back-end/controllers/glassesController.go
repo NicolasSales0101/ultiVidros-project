56d35da2aa178104b46067e5954fc73c97b8fb62
NicolasSales0101 <nicolascavalcante0101@gmail.com>
2022-04-05 18:41:52 -0400

    Início no roteamento e nas controllers de vidros

--- back-end/controllers/glassesController.go
+++ back-end/controllers/glassesController.go
@@ -1,10 +1,110 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/NicolasSales0101/ultiVidros-project/back-end/database"
+	"github.com/NicolasSales0101/ultiVidros-project/back-end/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ShowGlass(fctx *fiber.Ctx) error {
+
+	id := fctx.Params("id")
+
+	db := database.GetDatabase()
+
+	var glass models.Glass
+	err := db.First(&glass, id).Error
+	if err != nil {
+		log.Println("Error in method get ShowGlass (specific id in url params): ", err.Error())
+		return fctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot find book: " + err.Error(),
+		})
+	}
+
+	// Attention here
+	return fctx.Status(fiber.StatusOK).JSON(glass)
+}
 
 func ShowGlasses(fctx *fiber.Ctx) error {
-	return fctx.JSON(fiber.Map{
-		"status": "ok",
-	})
+	db := database.GetDatabase()
 
+	var glasses []models.Glass
+
+	err := db.Find(&glasses).Error
+	if err != nil {
+		log.Println("Error in method get ShowGlasses: ", err)
+		return fctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot list glasses: " + err.Error(),
+		})
+	}
+
+	return fctx.Status(fiber.StatusOK).JSON(glasses)
+}
+
+func CreateGlass(fctx *fiber.Ctx) error {
+	db := database.GetDatabase()
+
+	var glass models.Glass
+
+	if err := fctx.BodyParser(glass); err != nil {
+		log.Println("Error in method post CreateGlass: ", err)
+		return fctx.Status(fiber.StatusNotImplemented).JSON(fiber.Map{
+			"error": "cannot trasnform JSON to struct: " + err.Error(),
+		})
+	}
+
+	err := db.Create(&glass).Error
+	if err != nil {
+		log.Println("Error in method post CreateGlass: ", err)
+		return fctx.Status(fiber.StatusNotImplemented).JSON(fiber.Map{
+			"error": "cannot create glass: " + err.Error(),
+		})
+	}
+
+	return fctx.Status(fiber.StatusCreated).JSON(glass)
+
+}
+
+func UpdateGlass(fctx *fiber.Ctx) error {
+	db := database.GetDatabase()
+
+	var glass models.Glass
+
+	if err := fctx.BodyParser(glass); err != nil {
+		log.Println("Error in method post UpdateGlass: ", err)
+		return fctx.Status(fiber.StatusNotImplemented).JSON(fiber.Map{
+			"error": "cannot trasnform JSON to struct: " + err.Error(),
+		})
+	}
+
+	err := db.Save(&glass).Error
+	if err != nil {
+		log.Println("Error in method post CreateGlass: ", err)
+		return fctx.Status(fiber.StatusNotImplemented).JSON(fiber.Map{
+			"error": "cannot update glass: " + err.Error(),
+		})
+	}
+
+	return fctx.Status(fiber.StatusCreated).JSON(glass)
+}
+
+func DeleteGlass(fctx *fiber.Ctx) error {
+
+	id := fctx.Params("id")
+
+	db := database.GetDatabase()
+
+	err := db.Delete(&models.Glass{}, id).Error
+	if err != nil {
+		log.Println("Error in method delete DeleteGlass:", err.Error())
+		return fctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot delete glass" + err.Error(),
+		})
+	}
+
+	return fctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": "Glass removed succefully",
+	})
 }
--- back-end/routes/router.go
+++ back-end/routes/router.go
@@ -8,8 +8,13 @@ import (
 func ConfigRoutes(router *fiber.App) *fiber.App {
 	main := router.Group("/api/v1")
 
+	// General glasses routes
 	glasses := main.Group("/glasses")
+	glasses.Get("/:id", controllers.ShowGlass)
 	glasses.Get("/", controllers.ShowGlasses)
+	glasses.Post("/", controllers.CreateGlass)
+	glasses.Put("/", controllers.UpdateGlass)
+	glasses.Delete("/:id", controllers.DeleteGlass)
 
 	return router
 }
